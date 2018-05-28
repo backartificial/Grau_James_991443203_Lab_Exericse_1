b1c1baf32c970c348f9c763823601b0f416291fd
James Grau <backartificial@users.noreply.github.com>
2018-05-28 19:55:19 -0400

    Added in VS Vinal Lab
    Have Everything finalized and just committing to GitHub.

---
+++ CurrencyConverterApp/Currency_Coverter.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CurrencyConverterApp {
+    partial class frmCurrencyConverter {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblFormTitle = new System.Windows.Forms.Label();
+            this.lblValueTitle = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCAD = new System.Windows.Forms.Label();
+            this.cmbConvertTo = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFormTitle
+            // 
+            this.lblFormTitle.AutoSize = true;
+            this.lblFormTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFormTitle.Location = new System.Drawing.Point(43, 9);
+            this.lblFormTitle.Name = "lblFormTitle";
+            this.lblFormTitle.Size = new System.Drawing.Size(270, 31);
+            this.lblFormTitle.TabIndex = 0;
+            this.lblFormTitle.Text = "Currency Converter";
+            // 
+            // lblValueTitle
+            // 
+            this.lblValueTitle.AutoSize = true;
+            this.lblValueTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValueTitle.Location = new System.Drawing.Point(31, 54);
+            this.lblValueTitle.Name = "lblValueTitle";
+            this.lblValueTitle.Size = new System.Drawing.Size(76, 20);
+            this.lblValueTitle.TabIndex = 2;
+            this.lblValueTitle.Text = "Amount:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(113, 51);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(170, 26);
+            this.txtAmount.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Conver To:";
+            // 
+            // lblCAD
+            // 
+            this.lblCAD.AutoSize = true;
+            this.lblCAD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCAD.Location = new System.Drawing.Point(285, 54);
+            this.lblCAD.Name = "lblCAD";
+            this.lblCAD.Size = new System.Drawing.Size(46, 20);
+            this.lblCAD.TabIndex = 5;
+            this.lblCAD.Text = "CAD";
+            // 
+            // cmbConvertTo
+            // 
+            this.cmbConvertTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbConvertTo.FormattingEnabled = true;
+            this.cmbConvertTo.Location = new System.Drawing.Point(113, 89);
+            this.cmbConvertTo.Name = "cmbConvertTo";
+            this.cmbConvertTo.Size = new System.Drawing.Size(212, 28);
+            this.cmbConvertTo.TabIndex = 6;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(219, 123);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(106, 43);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 133);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Visible = false;
+            // 
+            // frmCurrencyConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 178);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.cmbConvertTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblValueTitle);
+            this.Controls.Add(this.lblFormTitle);
+            this.Controls.Add(this.lblCAD);
+            this.Name = "frmCurrencyConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Currency Converter";
+            this.Load += new System.EventHandler(this.Currency_Coverter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFormTitle;
+        private System.Windows.Forms.Label lblValueTitle;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCAD;
+        private System.Windows.Forms.ComboBox cmbConvertTo;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ CurrencyConverterApp/Currency_Coverter.cs
@@ -0,0 +1,120 @@
+﻿/**
+ * 
+ * File: Currency_Converter.cs
+ * Date: May 27, 2018
+ * Developer: James Grau
+ * Student ID: 991443203 
+ * 
+**/
+
+// Include the Needed Packages
+using System;
+using System.Windows.Forms;
+
+// Is the Projects Namespace
+namespace CurrencyConverterApp {
+    // Derive this form from the base Form Class
+    public partial class frmCurrencyConverter : Form {
+
+        // This method is used to initialize the form
+        public frmCurrencyConverter() {
+            // Initialize the form
+            InitializeComponent();
+        }
+
+        /**
+         * 
+         * This method is called when the form is loaded to load all of the options into the combobox
+         * 
+         **/
+        private void Currency_Coverter_Load(object sender, EventArgs e) {
+            // Add each "Convert To" Option into it's respective combobox
+            cmbConvertTo.Items.Add("Australian Dollar (AUD)");
+            cmbConvertTo.Items.Add("European Euro (EUR)");
+            cmbConvertTo.Items.Add("Jamaican Dollar (JMD)");
+            cmbConvertTo.Items.Add("Philippine Peso (PHP)");
+            cmbConvertTo.Items.Add("United States Dollar (USD)");            
+        }
+
+        /**
+         * 
+         * This method is used to add an action to perform the conversion when the convert button is pressed
+         * 
+         **/
+        private void btnConvert_Click(object sender, EventArgs e) {
+            // Initialize needed variables
+            double CADAmount, newAmount = 0;
+            string srtForm = "";
+
+            // Check if nothing and non number entered as amount value
+            if (txtAmount.Text.Length  <= 0 || double.TryParse(txtAmount.Text, out CADAmount) == false) {
+                // Display error message
+                MessageBox.Show("Oops.. That is an invalid amount.  Please enter a number.");
+
+                // Return nothing to exit our of method
+                return;
+            }
+
+            // Check if a "convert to" option is not selected
+            if (cmbConvertTo.SelectedIndex == -1) {
+                // Display an error message
+                MessageBox.Show("Oops... You need to select a conver to value in order to convert entered amount.  Please try again.");
+
+                // Return nothing to get out of method
+                return;
+            }
+            
+            // After input/selection validation, based on the selected "convert to" value, switch the case
+            switch (cmbConvertTo.SelectedIndex) {
+                // Case for index 0
+                case 0:
+                    // Calculate the new value
+                    newAmount = CADAmount * 1.02;
+
+                    // Set the short form surrency identifier
+                    srtForm = "AUD";
+                break;
+
+                // Case for index 1
+                case 1:
+                    // Calculate the new value
+                    newAmount = CADAmount * 0.66;
+
+                    // Set the short form surrency identifier
+                    srtForm = "EUR";
+                break;
+
+                // Case for index 2
+                case 2:
+                    // Calculate the new value
+                    newAmount = CADAmount * 97.31;
+
+                    // Set the short form surrency identifier
+                    srtForm = "JMD";
+                break;
+
+                // Case for index 3
+                case 3:
+                    // Calculate the new value
+                    newAmount = CADAmount * 40.47;
+
+                    // Set the short form surrency identifier
+                    srtForm = "PHP";
+                break;
+
+                // Case for index 4
+                case 4:
+                    // Calculate the new value
+                    newAmount = CADAmount * 0.77;
+
+                    // Set the short form surrency identifier
+                    srtForm = "USD";
+                break;
+            }
+
+            // Display the conversion lbl and then set it to the correct conversion value
+            lblResult.Visible = true;
+            lblResult.Text = string.Format("{0:C}", CADAmount) + " CAD is \n" + string.Format("{0:C}", newAmount) + " " + srtForm;
+        }
+    }
+}
